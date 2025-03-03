9f483ba8d3c9ccd38558dd4de217bc1e95319c23
Borys Lis <boryslis.katowice@gmail.com>
2025-03-03 11:24:32 +0100

    Last changes Note Change the email and password in feature

--- src/test/java/steps/AdressSteps.java
+++ src/test/java/steps/AdressSteps.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
+import pages.NewAdressPage;
 import pages.YourAccountPage;
 
 
@@ -16,6 +17,7 @@ public class AdressSteps {
     private WebDriver driver;
     private LoginPage loginPage;
     private YourAccountPage yourAccountPage;
+    private NewAdressPage newAdressPage;
 
     @Given("I am on the mystore login page")
     public void openLoginPage() {
@@ -42,7 +44,7 @@ public class AdressSteps {
 
     @When("I click on the Addresses tile")
     public void clickAddFirstAddress() {
-     yourAccountPage.clickAddNewAdress();
+        yourAccountPage.clickAddNewAdress();
     }
 
 
@@ -55,6 +57,21 @@ public class AdressSteps {
 
     @When("I fill the address form with alias {string}, address {string}, city {string}, zip {string}, country {string}, phone {string}")
     public void fillTheAddNewAddressForm(String alias, String address, String city, String zip, String country, String phone) {
-
+        newAdressPage = new NewAdressPage(driver);
+        newAdressPage.fillAddressForm(alias, address, city, zip, country, phone);
     }
+
+    @Then("The new address should be added with alias {string}, address {string}, city {string}, zip {string}, country {string}, phone {string}")
+    public void theNewAddressShouldBeAddedWithProvidedData(String alias, String address, String city, String zip, String country, String phone) {
+        String addressText = newAdressPage.getAddedAddressText();
+        Assert.assertTrue("Alias not found", addressText.contains(alias));
+        Assert.assertTrue("Address not found", addressText.contains(address));
+        Assert.assertTrue("City not found", addressText.contains(city));
+        Assert.assertTrue("Zip not found", addressText.contains(zip));
+        Assert.assertTrue("Country not found", addressText.contains(country));
+        Assert.assertTrue("Phone not found", addressText.contains(phone));
+    }
+
+
 }
+
