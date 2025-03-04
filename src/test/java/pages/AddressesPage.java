8c834b9d453fe3606c00fb26503dcfae3f084474
Borys Lis <boryslis.katowice@gmail.com>
2025-03-04 12:03:39 +0100

    Added AddressesPage for saved address

---
+++ src/test/java/pages/AddressesPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AddressesPage {
+
+
+    private  WebDriver driver;
+
+    @FindBy(partialLinkText = "Create new address")
+    private WebElement createNewAddressLink;
+
+    public AddressesPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
