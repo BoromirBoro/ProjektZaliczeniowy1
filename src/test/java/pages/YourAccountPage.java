519ee891ee741c1a3d87f5f07d4b1497892dce7d
Borys Lis <boryslis.katowice@gmail.com>
2025-03-04 15:06:12 +0100

    Added AddressesPage for saved address

--- src/test/java/pages/YourAccountPage.java
+++ src/test/java/pages/YourAccountPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -26,14 +25,6 @@ public class YourAccountPage {
     private WebElement addressesButton;
 
     public void clickAddressOption() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        if (wait.until(ExpectedConditions.elementToBeClickable(addFirstAddressButton)).isDisplayed()) {
-            addFirstAddressButton.click();
-        } else if (wait.until(ExpectedConditions.elementToBeClickable(addressesButton)).isDisplayed()) {
-            addressesButton.click();
-        } else {
-            throw new RuntimeException("Neither 'Add First Address' button nor 'Addresses' button is available or clickable.");
-        }
+        addressesButton.click();
     }
 }
